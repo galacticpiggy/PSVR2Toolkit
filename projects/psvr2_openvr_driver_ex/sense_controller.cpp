dab5124e09a77e9d83fe74617e89da47498018d3
galacticpiggy <firebycloud@gmail.com>
2025-12-12 09:39:31 -0600

    Update sense_controller.cpp
    Reverted changes from https://github.com/BnuuySolutions/PSVR2Toolkit/commit/39b4832

--- projects/psvr2_openvr_driver_ex/sense_controller.cpp
+++ projects/psvr2_openvr_driver_ex/sense_controller.cpp
@@ -23,11 +23,12 @@ SenseController SenseController::rightController = SenseController(false);
 
 std::atomic<std::thread*> hapticsThread;
 
-void SenseController::SetGeneratedHaptic(float freq, uint32_t amp, uint32_t sampleCount) {
+void SenseController::SetGeneratedHaptic(float freq, uint32_t amp, uint32_t sampleCount, bool phaseJump) {
   std::scoped_lock<std::mutex> lock(controllerMutex);
 
   this->hapticFreq = freq;
   this->hapticAmp = amp;
+  this->phaseJump = phaseJump;
   this->hapticSamplesLeft = sampleCount;
 }
 void SenseController::SetPCM(const std::vector<int8_t>& newPCMData) {
@@ -82,7 +83,7 @@ void SenseController::SetHandle(void* handle, int padHandle) {
 
   if (handle != nullptr)
   {
-    this->SetGeneratedHaptic(800.0f, k_unSenseMaxHapticAmplitude, 1500);
+    this->SetGeneratedHaptic(800.0f, k_unSenseMaxHapticAmplitude, 1500, false);
     this->ClearTimestampOffset();
   }
 }
@@ -147,16 +148,36 @@ void SenseController::SendToDevice() {
         this->samplesRead += bytesToCopy;
       }
 
+      // We basically want to make a thud
+      if (this->phaseJump)
+      {
+        this->phaseJump = false;
+        hapticPosition = hapticPosition > k_unSenseHalfSamplePosition ? 0 : k_unSenseHalfSamplePosition;
+
+        bool isStartingAtUp = hapticPosition == 0;
+
+        int8_t amp = static_cast<int8_t>(std::min(this->hapticAmp, static_cast<uint32_t>(k_unSenseMaxHapticAmplitude)));
+
+        for (int i = 0; i < 6; i++)
+          buffer.hapticPCM[i] = ClampedAdd(buffer.hapticPCM[i], isStartingAtUp ? amp : -amp);
+
+        for (int i = 6; i < 12; i++)
+          buffer.hapticPCM[i] = ClampedAdd(buffer.hapticPCM[i], isStartingAtUp ? -amp : amp);
+
+        // Back up a bit that so the actuator can move down in 6 samples, then all the way up.
+        hapticPosition -= 6;
+      }
+
       // Calculate the haptic overdrive based on frequency. We want overdrive to range from 25.0 to 1.0.
       // Basically, this makes a square wave from the cosine wave. Lower frequencies will have a higher overdrive.
-      // We also overdrive for frequencies above 300 Hz.
+      // We also overdrive for frequencies above 500 Hz.
 
       double overdrive = 25.0;
 
       // Make sure we don't divide by zero
       if (this->hapticFreq != 0.0)
       {
-        overdrive = Clamp(1000.0 / this->hapticFreq, 10.0 + 1.0, 35.0) - 10.0 + (this->hapticFreq - 300.0);
+        overdrive = Clamp(1000.0 / this->hapticFreq, 10.0 + 1.0, 35.0) - 10.0 + (this->hapticFreq - 500.0);
       }
 
       // In addition to copying the PCM data, we also want to add the generated haptic data to the buffer.
@@ -170,10 +191,6 @@ void SenseController::SendToDevice() {
           this->hapticSamplesLeft -= 1;
         }
       }
-
-      if (this->hapticSamplesLeft == 0) {
-        hapticPosition = hapticPosition > k_unSenseHalfSamplePosition ? 0 : k_unSenseHalfSamplePosition;
-      }
     }
 
     buffer.settings.timeStampMicrosecondsLastSend = static_cast<uint32_t>(GetHostTimestamp());
@@ -182,8 +199,9 @@ void SenseController::SendToDevice() {
   }
 
   long pendingOperationCount = asyncWriter.GetPendingOperationCount();
-  if (pendingOperationCount > 3)
+  if (pendingOperationCount > 2)
   {
+    Util::DriverLog("Failed to send. Too many pending operations.\r\n");
     return;
   }
 
@@ -195,7 +213,7 @@ void SenseController::SendToDevice() {
     this->SetHandle(NULL, -1);
   };
 
-  if (!asyncWriter.Write(this->handle, &buffer, sizeof(buffer), 5000, timeoutLambda))
+  if (!asyncWriter.Write(this->handle, &buffer, 78, 5000, timeoutLambda))
   {
     CloseHandle(this->handle);
 
@@ -218,7 +236,7 @@ void SenseThread()
   QueryPerformanceFrequency(&frequency);
 
   // Duration we want to run every iteration (32/3000 or 0.010666 seconds)
-  LONGLONG duration = static_cast<LONGLONG>((32.0 / static_cast<double>(k_unSenseSampleRate)) * frequency.QuadPart);
+  LONGLONG duration = static_cast<LONGLONG>((32.0 / 3000.0) * frequency.QuadPart);
 
   LARGE_INTEGER start;
   QueryPerformanceCounter(&start);
@@ -317,17 +335,13 @@ static void PollNextEvent(vr::VREvent_t* pEvent)
     uint8_t senseHapticAmp = 0;
     uint32_t senseHapticSamplesLeft = 0;
 
+    // Phase jump to make a "thud."
+    bool phaseJump = hapticEvent.fDurationSeconds == 0.0f;
+
     if (hapticEvent.fAmplitude != 0.0f)
     {
       senseHapticAmp = static_cast<uint8_t>(sqrtf(hapticEvent.fAmplitude) * k_unSenseMaxHapticAmplitude);
-      if (hapticEvent.fDurationSeconds == 0.0f) {
-        senseHapticFreq = std::max(80.0f, senseHapticFreq);
-        senseHapticSamplesLeft = static_cast<uint32_t>(k_unSenseSampleRate / senseHapticFreq);
-      }
-      else
-      {
-        senseHapticSamplesLeft = static_cast<uint32_t>(hapticEvent.fDurationSeconds * k_unSenseSampleRate);
-      }
+      senseHapticSamplesLeft = phaseJump ? 96 : 16 + static_cast<uint32_t>(hapticEvent.fDurationSeconds * k_unSenseSampleRate);
     }
 
     SenseController* controller = nullptr;
@@ -343,7 +357,7 @@ static void PollNextEvent(vr::VREvent_t* pEvent)
 
     if (controller != nullptr)
     {
-      controller->SetGeneratedHaptic(senseHapticFreq, senseHapticAmp, senseHapticSamplesLeft);
+      controller->SetGeneratedHaptic(senseHapticFreq, senseHapticAmp, senseHapticSamplesLeft, phaseJump);
     }
 
     break;
@@ -355,12 +369,27 @@ static void PollNextEvent(vr::VREvent_t* pEvent)
   }
 }
 
+void psvr2_toolkit::StartSenseThread() {
+  hapticsThread = new std::thread(SenseThread);
+  hapticsThread.load()->detach();
+}
+
+void psvr2_toolkit::StopSenseThread() {
+  std::thread* hapticsThreadCopy = hapticsThread;
+  hapticsThread = nullptr;
+
+  if (hapticsThreadCopy->joinable())
+  {
+    hapticsThreadCopy->join();
+  }
+  delete hapticsThreadCopy;
+}
+
 void SenseController::Initialize()
 {
   DriverHostProxy::Instance()->SetEventHandler(PollNextEvent);
 
-  hapticsThread = new std::thread(SenseThread);
-  hapticsThread.load()->detach();
+  StartSenseThread();
 }
 
 void SenseController::Destroy()
@@ -371,12 +400,5 @@ void SenseController::Destroy()
   auto& rightController = SenseController::GetRightController();
   rightController.SetHandle(NULL, -1);
 
-  std::thread* hapticsThreadCopy = hapticsThread;
-  hapticsThread = nullptr;
-
-  if (hapticsThreadCopy->joinable())
-  {
-    hapticsThreadCopy->join();
-  }
-  delete hapticsThreadCopy;
+  StopSenseThread();
 }
